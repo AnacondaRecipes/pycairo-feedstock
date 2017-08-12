baa428b43930c94f34c1b9ccf115e40b7a429594
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-08-12 14:44:14 +0000

    WIP: CDT Wars

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+SYSROOT="${PREFIX}/$(${CC} -dumpmachine)/sysroot"
+
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${SYSROOT}/usr/lib64/pkgconfig:${SYSROOT}/usr/share/pkgconfig"
+
 if [ $(uname) = Darwin ] ; then
     # This needs to be kept the same as what was used to build Cairo, which is
     # apparently this:
