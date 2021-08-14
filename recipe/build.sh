bbb28bd870e431300c629c009b4f313e0dc56ac8
Kai Tietz <ktietz@anaconda.com>
2021-08-14 10:49:17 +0200

    update ref

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,11 @@
 set -e
 
 if [ $(uname) = Darwin ] ; then
+    if [[ ${target_platform} == osx-64 ]]; then
     # This needs to be kept the same as what was used to build Cairo, which is
     # apparently this:
     export MACOSX_DEPLOYMENT_TARGET=10.9
+    fi
 fi
 
 python setup.py install
