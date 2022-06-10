a3a3838b08471128bbfb2d7757ed0266d0525c69
Charles Bousseau <cbousseau@anaconda.com>
2022-06-10 14:59:21 -0400

    update to 1.21.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,25 @@
 
 set -e
 
-if [ $(uname) = Darwin ] ; then
-    if [[ ${target_platform} == osx-64 ]]; then
-    # This needs to be kept the same as what was used to build Cairo, which is
-    # apparently this:
-    export MACOSX_DEPLOYMENT_TARGET=10.9
-    fi
-fi
+# We're using the meson build system as opposed to 'pip install .' because it provides
+# the pkg-config files for `pycairo`. These are used by downstream packages (notably
+# `pygobject`) that also use the meson build system in order to locate `pycairo`.
+# Without them, `pycairo` will not be found and might be built as an in-tree subproject
+# which is obviously undesirable.
 
-python setup.py install
+# meson options
+meson_config_args=(
+  --prefix="$PREFIX"
+  --wrap-mode=nofallback
+  --buildtype=release
+  --backend=ninja
+  -Dlibdir=lib
+  -D python="$PYTHON"
+)
+
+# configure build using meson
+meson setup builddir ${MESON_ARGS} "${meson_config_args[@]}"
+
+meson compile -v -C builddir -j ${CPU_COUNT}
+meson test -C builddir --print-errorlogs --timeout-multiplier 10 --num-processes ${CPU_COUNT}
+meson install -C builddir
