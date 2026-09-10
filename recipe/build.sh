588437229c60c23431c51ce8560e36ee68a594a3
imykhailov <imykhailov@anaconda.com>
2026-09-10 16:22:03 +0300

    fix: add missing ninja-base build dep, move meson to host, fix linter false-positive comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# We're using the meson build system as opposed to 'pip install .' because it provides
+# We're using the meson build system instead of pip because it provides
 # the pkg-config files for `pycairo`. These are used by downstream packages (notably
 # `pygobject`) that also use the meson build system in order to locate `pycairo`.
 # Without them, `pycairo` will not be found and might be built as an in-tree subproject
